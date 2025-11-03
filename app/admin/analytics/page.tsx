498719cda757bd2f15ea23e5e518d96f3ea2516e
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-11-03 07:29:15 +0600

    "feat: add analytics page component"

---
+++ app/admin/analytics/page.tsx
@@ -0,0 +1,10 @@
+import AnalyticsDashboard from '../components/AnalyticsDashboard'
+
+export const metadata = {
+  title: 'Analytics | Nova Furnishings',
+  description: 'View store analytics and reports',
+}
+
+export default function AnalyticsPage() {
+  return <AnalyticsDashboard />
+}
